2338d70aa8c99ed11999aba31ecf0a446501e8ef
gio54321 <g.dellimmagine@gmail.com>
2022-10-25 00:01:04 +0200

    Add wait for Kubesec container to be running

--- k8spurifier/builtin_analyses/kubesec_analysis.py
+++ k8spurifier/builtin_analyses/kubesec_analysis.py
@@ -1,7 +1,7 @@
-from typing import Dict, List, Mapping
+from time import sleep
+from typing import List, Mapping
 from k8spurifier.analysis import AnalysisResult, StaticAnalysis
 from loguru import logger
-import subprocess
 import json
 import docker
 import requests
@@ -20,16 +20,18 @@ class KubesecIntegrationAnalysis(StaticAnalysis):
     def run_analysis(self, input_objects: Mapping[str, List[ApplicationObject]]) -> List[AnalysisResult]:
         kubernetes_objects = input_objects.get('kubernetes_config')
 
-        docker_client = docker.from_env()
+        self.docker_client = docker.from_env()
 
         # spawn kubesec container
         logger.debug('spawning kubesec container')
-        kubesec_container = docker_client.containers.run(
+        kubesec_container = self.docker_client.containers.run(
             'kubesec/kubesec:v2', detach=True, ports={'8080/tcp': 8080})
 
+        output_results = []
         try:
+            self.wait_for_running_container(kubesec_container)
+
             seen_paths = set()
-            output_results = []
             for obj in kubernetes_objects:
                 if str(obj.path) in seen_paths:
                     continue
@@ -37,7 +39,7 @@ class KubesecIntegrationAnalysis(StaticAnalysis):
 
                 logger.debug(f"processing kubernetes config {obj.path.name}")
 
-                results = self.process_kubernetes_config(obj)
+                results = self.__process_kubernetes_config(obj)
                 output_results += results
         finally:
             kubesec_container.stop()
@@ -45,7 +47,7 @@ class KubesecIntegrationAnalysis(StaticAnalysis):
 
         return output_results
 
-    def process_kubernetes_config(self, obj: ApplicationObject):
+    def __process_kubernetes_config(self, obj: ApplicationObject):
         with open(obj.path, 'rb') as f:
             raw_data = f.read()
         response = requests.post(
@@ -71,3 +73,15 @@ class KubesecIntegrationAnalysis(StaticAnalysis):
                 results.append(
                     AnalysisResult(description, {Smell.UPM}))
         return results
+
+    def wait_for_running_container(self, container):
+        timeout = 120
+        stop_time = 0.5
+        elapsed_time = 0
+
+        while elapsed_time < timeout:
+            sleep(stop_time)
+            elapsed_time += stop_time
+            container.reload()
+            if container.status == 'running':
+                return
