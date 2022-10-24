985a1dec6a9d1de94be62987d3f8b933d29e66f0
gio54321 <g.dellimmagine@gmail.com>
2022-10-24 23:44:16 +0200

    Improve Kubesec container handling

--- k8spurifier/builtin_analyses/kubesec_analysis.py
+++ k8spurifier/builtin_analyses/kubesec_analysis.py
@@ -3,6 +3,8 @@ from k8spurifier.analysis import AnalysisResult, StaticAnalysis
 from loguru import logger
 import subprocess
 import json
+import docker
+import requests
 
 
 from k8spurifier.applicationobject import ApplicationObject
@@ -18,36 +20,54 @@ class KubesecIntegrationAnalysis(StaticAnalysis):
     def run_analysis(self, input_objects: Mapping[str, List[ApplicationObject]]) -> List[AnalysisResult]:
         kubernetes_objects = input_objects.get('kubernetes_config')
 
-        seen_paths = set()
-        output_results = []
-        for obj in kubernetes_objects:
-            if str(obj.path) in seen_paths:
-                continue
-            seen_paths.add(str(obj.path))
+        docker_client = docker.from_env()
 
-            logger.debug(f"processing kubernetes config {obj.path.name}")
+        # spawn kubesec container
+        logger.debug('spawning kubesec container')
+        kubesec_container = docker_client.containers.run(
+            'kubesec/kubesec:v2', detach=True, ports={'8080/tcp': 8080})
 
-            command = f'docker run -i kubesec/kubesec:v2 scan /dev/stdin < {obj.path}'
-            result = subprocess.run(command, shell=True, capture_output=True)
-            response = result.stdout.decode()
-
-            data = json.loads(response)
-
-            for res_obj in data:
-                scoring = res_obj.get('scoring')
-                if scoring is None:
+        try:
+            seen_paths = set()
+            output_results = []
+            for obj in kubernetes_objects:
+                if str(obj.path) in seen_paths:
                     continue
+                seen_paths.add(str(obj.path))
 
-                advices = scoring.get('advise')
-                if advices is None:
-                    continue
+                logger.debug(f"processing kubernetes config {obj.path.name}")
 
-                for advice in advices:
-                    description = f"Kubesec.io found potential problems in {obj.path.name}\n" +\
-                        f"selector: {advice.get('selector')}\n" +\
-                        f"reason: {advice.get('reason')}"
-
-                    output_results.append(
-                        AnalysisResult(description, {Smell.UPM}))
+                results = self.process_kubernetes_config(obj)
+                output_results += results
+        finally:
+            kubesec_container.stop()
+            kubesec_container.remove()
 
         return output_results
+
+    def process_kubernetes_config(self, obj: ApplicationObject):
+        with open(obj.path, 'rb') as f:
+            raw_data = f.read()
+        response = requests.post(
+            'http://localhost:8080/scan', data=raw_data)
+
+        data = json.loads(response.text)
+
+        results = []
+        for res_obj in data:
+            scoring = res_obj.get('scoring')
+            if scoring is None:
+                continue
+
+            advices = scoring.get('advise')
+            if advices is None:
+                continue
+
+            for advice in advices:
+                description = f"Kubesec.io found potential problems in {obj.path.name}\n" +\
+                    f"selector: {advice.get('selector')}\n" +\
+                    f"reason: {advice.get('reason')}"
+
+                results.append(
+                    AnalysisResult(description, {Smell.UPM}))
+        return results
