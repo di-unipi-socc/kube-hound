7a459a03911b7effa40f9c12874c5382de1b37fc
gio54321 <g.dellimmagine@gmail.com>
2022-11-04 09:42:20 +0100

    Refactor External-IP analysis

--- k8spurifier/builtin_analyses/externalip_analysis.py
+++ k8spurifier/builtin_analyses/externalip_analysis.py
@@ -9,9 +9,9 @@ from k8spurifier.smells import Smell
 
 
 class ExternalIpAnalysis(DynamicAnalysis):
-    analysis_id = 'D0'
-    analysis_name = 'Secrets in environment variables analysis'
-    analysis_description = 'detect hardcoded secrets in containers environment'
+    analysis_id = 'external_ip'
+    analysis_name = 'Exernal-IP analysis'
+    analysis_description = 'detect exposed services by analysing the External-IP field for services'
     input_types: List[str] = ['kubernetes_config']
 
     def run_analysis(self, input_objects: Mapping[str, List[ApplicationObject]]) \
@@ -28,7 +28,8 @@ class ExternalIpAnalysis(DynamicAnalysis):
                 service_properties = document.service_properties
 
                 services_mapping[service_name] = service_properties
-        output_results = []
+
+        output_smells = []
 
         v1 = client.CoreV1Api()
         ret = v1.list_service_for_all_namespaces(watch=False)
@@ -40,23 +41,33 @@ class ExternalIpAnalysis(DynamicAnalysis):
 
         for service in data:
             service_name = service['metadata']['name']
+            logger.debug(f"checking service {service_name}")
 
             status = service['status']
             load_balancer_status = status.get(
                 'load_balancer').get('ingress')
-            if load_balancer_status is not None:
-                if service_name in services_mapping:
-                    logger.debug(f"checking service {service_name}")
+            if load_balancer_status is not None and len(load_balancer_status) > 0:
+                if str(service_name) in services_mapping:
                     properties = services_mapping[service_name]
-                    if properties is None:
-                        continue
-                    external = properties.get('external')
-                    if external is False:
-                        description = f"External service detected: {service_name}\n" +\
-                            f"exposed on external ip {load_balancer_status.get('ip')}" +\
-                            f"on host {load_balancer_status.get('host')}"
 
-                    output_results.append(
+                    logger.debug(properties)
+                    if properties is None:
+                        external = False
+                    else:
+                        external = properties.get('external')
+
+                    if external is True:
+                        continue
+
+                    load_balancer_info = load_balancer_status[0]
+                    ip = load_balancer_info.get('ip')
+                    host = load_balancer_info.get('host')
+
+                    description = f"External service detected: {service_name}\n" +\
+                        f"exposed on external ip {ip if ip is not None else 'unknown'}, " +\
+                        f"on host {host if host is not None else 'unknown'}"
+
+                    output_smells.append(
                         AnalysisResult(description, {Smell.PAM}))
-            # print(data[service])
-        return output_results
+
+        return output_smells
