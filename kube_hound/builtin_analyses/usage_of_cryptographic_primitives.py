16e1a29e22c66d40eb6af16c0182d701875bd11d
Howard-Grubb Thomas <t.howardgrubb@campus.unimib.it>
2023-08-21 14:48:11 +0200

    split the plugin into different function

--- kube_hound/builtin_analyses/usage_of_cryptographic_primitives.py
+++ kube_hound/builtin_analyses/usage_of_cryptographic_primitives.py
@@ -23,9 +23,99 @@ class UsageOfCryptographicPrimitives(StaticAnalysis):
 
     def run_analysis(self, input_objects: Mapping[str, List[ApplicationObject]]) \
             -> List[AnalysisResult]:
+        try:
+            volumes = self.build_volume(input_objects)
+
+            self.docker_client = docker.from_env()
+
+            url = 'http://localhost:9000'
+            username = "admin"
+            password = "admin"
+            host_properties_path = str(Path('./custom-sonar.properties').absolute())
+            container_properties_path = '/opt/sonarqube/conf/custom.properties'
+
+            output_results = []
+
+            # set project variables
+            project_key = "my_project"
+            project_name = "My Project"
+            project_visibility = "public"
+            auth = ("admin", "admin")
+
+            # various api enpoints that are needed
+            api_endpoint_create = f"{url}/api/projects/create"  # to create project
+            api_endpoint_create_token = f"{url}/api/user_tokens/generate"  # to generate user token
+            api_endpoint_search = f"{url}/api/issues/search"  # to search through issue key
+
+            # sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)
+
+            sonarqube_container = self.spawn_sonarqube_container(host_properties_path, container_properties_path)
+            try:
+                self.wait_for_running_container(sonarqube_container)
+                logger.debug("Sonarqube container is now running")
+                logger.debug("Creating sonarqube server...")
+                while True:
+                    container_logs = self.docker_client.containers.get(str(sonarqube_container.id)).logs().decode(
+                        "utf-8")
+                    if "SonarQube is operational" in container_logs:
+                        break
+
+                    sleep(1)  # Wait for 1 second before checking again
+
+                logger.debug("Server is now running")
+
+                # sonar.auth.authenticate_user(login="admin", password="admin")
+                # sonar.auth.check_credentials()
+
+                self.create_sonarqube_project(project_name, project_key, project_visibility, api_endpoint_create, auth)
+
+                token = self.create_token( api_endpoint_create_token, auth)
+
+                # Inspect the container
+                container_info = self.docker_client.api.inspect_container(sonarqube_container.id)
+
+                # Get the IP address from the container's network settings
+                ip_address = container_info['NetworkSettings']['IPAddress']
+                access_address = 'http://' + str(ip_address) + ':9000'
+
+                sonarscanner_container = self.spawn_sonarscanner_container(volumes, access_address, project_key, token)
+
+                while True:
+                    sonarscanner_container.reload()
+                    if not sonarscanner_container.status == 'running':
+                        break
+
+                    sleep(1)  # Wait for 1 second before checking again
+
+                sleep(5)  # needs to wait a few seconds for the ruslts to actually be registered
+
+                rule_keys = ["python:S5542", "php:S5542", "javascript:S5542", "kotlin:S5542", "java:S5542",
+                             "typescript:S5542"]
+
+                self.api_request(project_key, rule_keys, api_endpoint_search, auth, output_results)
+
+                return output_results
+            except requests.exceptions.RequestException as e:
+                logger.warning("Error triggering analysis:", e)
+        finally:
+            # Stop the container
+
+            sonarscanner_container.stop()
+
+            # Remove the container
+            sonarscanner_container.remove()
+
+            # Stop the container
+            sonarqube_container.stop()
+
+            # Remove the container
+            sonarqube_container.remove()
+
+        return []
+
+    def build_volume(self, input_objects):
         assert 'sourcecode' in input_objects
         sourcecode_objects = input_objects['sourcecode']
-
 
         # build volume for sonarqube analysis (directories to be analyzed)
         paths = [str(sourcecode.path) for sourcecode in sourcecode_objects]
@@ -35,184 +125,95 @@ class UsageOfCryptographicPrimitives(StaticAnalysis):
                 'bind': '/usr/src' + path,
                 'mode': 'rw'
             }
+        return volumes
 
-        self.docker_client = docker.from_env()
-
-        url = 'http://localhost:9000'
-        username = "admin"
-        password = "admin"
-        host_properties_path = str(Path('./custom-sonar.properties').absolute())
-        container_properties_path = '/opt/sonarqube/conf/custom.properties'
-
-        output_results = []
-
-        #set project variables
-        project_key = "my_project"
-        project_name = "My Project"
-        project_visibility = "public"
-        auth = ("admin", "admin")
-
-        #various api enpoints that are needed
-        api_endpoint_create = f"{url}/api/projects/create"                  #to create project
-        api_endpoint_create_token = f"{url}/api/user_tokens/generate"       #to generate user token
-        api_endpoint_search = f"{url}/api/issues/search"                    #to search through issue key
-
-        #sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)
-
+    def spawn_sonarqube_container(self, host_properties_path, container_properties_path):
         # spawn a sonarqube container
         logger.debug('Spawning sonarqube container')
         sonarqube_container = self.docker_client.containers.run(
-            'sonarqube', detach=True, ports={'9000/tcp': 9000}, volumes={host_properties_path: {'bind': container_properties_path, 'mode': 'ro'}})
+            'sonarqube', detach=True, ports={'9000/tcp': 9000},
+            volumes={host_properties_path: {'bind': container_properties_path, 'mode': 'ro'}})
+        return sonarqube_container
 
+    def create_sonarqube_project(self, project_name, project_key, project_visibility, api_endpoint_create, auth):
+        # Set the request payload with the required parameters
+        payload = {
+            "name": project_name,
+            "project": project_key,
+            "visibility": project_visibility,
+        }
 
-        try:
-            self.wait_for_running_container(sonarqube_container)
-            logger.debug("Sonarqube container is now running")
-            logger.debug("Creating sonarqube server...")
-            while True:
-                container_logs = self.docker_client.containers.get(str(sonarqube_container.id)).logs().decode("utf-8")
-                if "SonarQube is operational" in container_logs:
-                    break
+        # Send the POST request to create the project
+        response = requests.post(api_endpoint_create, auth=auth, data=payload)
 
-                sleep(1)  # Wait for 1 second before checking again
+        # Check the response status code
+        if response.status_code == 200:
+            logger.debug("Sonarqube project created successfully.")
+        else:
+            logger.warning("Sonarqube project creation failed.")
+    def create_token(self, api_endpoint_create_token, auth):
+        # Set the request payload with the required parameters
+        payload = {
+            'name': 'My Token',
+            'login': 'admin',
+        }
 
-            logger.debug("Server is now running")
+        # Send the POST request to create the user token
+        response = requests.post(api_endpoint_create_token, auth=auth, data=payload)
 
-            #sonar.auth.authenticate_user(login="admin", password="admin")
-            #sonar.auth.check_credentials()
+        # Check the response status code
+        if response.status_code == 200:
+            data = response.json()
+            token = str(data['token'])
+            return token
+        else:
+            logger.warning('Failed to create the authentication token.')
 
+    def spawn_sonarscanner_container(self, volumes, access_address, project_key, token):
+        # spawn a sonarscanner container
+        logger.debug('Spawning sonarscanner container...')
+        sonarscanner_container = self.docker_client.containers.run(
+            'sonarsource/sonar-scanner-cli',
+            detach=True,
+            volumes=volumes,
+            environment={
+                'SONAR_HOST_URL': access_address,
+                'SONAR_SCANNER_OPTS': f'-Dsonar.projectKey={project_key} -Dsonar.token={token} -Dsonar.java.binaries=.',
+            },
+        )
+        return sonarscanner_container
 
+    def api_request(self, project_key, rule_keys, api_endpoint_search, auth, output_results):
+        # Parameters for the API request
+        params = {
+            "projectKey": project_key,
+            "rules": ",".join(rule_keys),
+        }
 
+        # Send the authenticated API request
+        response = requests.get(api_endpoint_search, params=params, auth=auth)
 
+        # Check if the request was successful
+        if response.status_code == 200:
+            response_json = response.json()
+            issues = response_json.get("issues", [])
 
-            # Set the request payload with the required parameters
-            payload = {
-                "name": project_name,
-                "project": project_key,
-                "visibility": project_visibility,
-            }
+            if issues:
+                for issue in issues:
+                    component = issue["component"]
+                    message = issue["message"]
+                    line = issue["line"]
+                    component = os.path.basename(component)
+                    description = f"Sonarqube found potential problems in {component}\n" + \
+                                  f"line: {line}\n" + \
+                                  f"reason: {message}"
 
-            # Send the POST request to create the project
-            response = requests.post(api_endpoint_create, auth=auth, data=payload)
-
-            # Check the response status code
-            if response.status_code == 200:
-                logger.debug("Sonarqube project created successfully.")
+                    output_results.append(
+                        AnalysisResult(description, {Smell.UCP}))
             else:
-                logger.warning("Sonarqube project creation failed.")
-
-
-
-
-
-            # Set the request payload with the required parameters
-            payload = {
-                'name': 'My Token',
-                'login': 'admin',
-            }
-
-            # Send the POST request to create the user token
-            response = requests.post(api_endpoint_create_token, auth=auth, data=payload)
-
-            # Check the response status code
-            if response.status_code == 200:
-                data = response.json()
-                token = str(data['token'])
-            else:
-                logger.warning('Failed to create the authentication token.')
-
-
-
-
-
-
-            # Inspect the container
-            container_info = self.docker_client.api.inspect_container(sonarqube_container.id)
-
-            # Get the IP address from the container's network settings
-            ip_address = container_info['NetworkSettings']['IPAddress']
-            access_address = 'http://'+str(ip_address)+':9000'
-
-
-
-            # spawn a sonarscanner container
-            logger.debug('Spawning sonarscanner container...')
-            sonarscanner_container = self.docker_client.containers.run(
-                'sonarsource/sonar-scanner-cli',
-                detach=True,
-                volumes=volumes,
-                environment={
-                    'SONAR_HOST_URL': access_address,
-                    'SONAR_SCANNER_OPTS': f'-Dsonar.projectKey={project_key} -Dsonar.token={token} -Dsonar.java.binaries=.',
-                },
-            )
-
-            while True:
-                sonarscanner_container.reload()
-                if not sonarscanner_container.status == 'running':
-
-                    break
-
-                sleep(1)  # Wait for 1 second before checking again
-
-            sleep(5) #needs to wait a few seconds for the ruslts to actually be registered
-
-            rule_keys = ["python:S5542", "php:S5542", "javascript:S5542", "kotlin:S5542", "java:S5542", "typescript:S5542"]
-
-            # Parameters for the API request
-            params = {
-                "projectKey": project_key,
-                "rules": ",".join(rule_keys),
-            }
-
-
-            # Send the authenticated API request
-            response = requests.get(api_endpoint_search, params=params, auth=auth)
-
-            # Check if the request was successful
-            if response.status_code == 200:
-                response_json = response.json()
-                issues = response_json.get("issues", [])
-
-                if issues:
-                    for issue in issues:
-                        component = issue["component"]
-                        message = issue["message"]
-                        line = issue["line"]
-                        component = os.path.basename(component)
-                        description = f"Sonarqube found potential problems in {component}\n" + \
-                                      f"line: {line}\n" + \
-                                      f"reason: {message}"
-
-                        output_results.append(
-                            AnalysisResult(description, {Smell.UCP}))
-                else:
-                    logger.info("No instances of usage of cryptographic primitves")
-            else:
-                logger.warning("Failed to retrieve issues.")
-
-
-
-
-            # Stop the container
-
-            sonarscanner_container.stop()
-
-            # Remove the container
-            sonarscanner_container.remove()
-
-
-            # Stop the container
-            sonarqube_container.stop()
-
-            # Remove the container
-            sonarqube_container.remove()
-
-            return output_results
-        except requests.exceptions.RequestException as e:
-            logger.warning("Error triggering analysis:", e)
-
-        return []
+                logger.info("No instances of usage of cryptographic primitves")
+        else:
+            logger.warning("Failed to retrieve issues.")
 
     def wait_for_running_container(self, container):
         """
